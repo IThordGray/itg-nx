62a82712b713a4bdf318c0168aaabcbf7ef28c35
Ivor Thord-Gray <ivor.thord-gray@iotnxt.com>
2019-09-09 08:51:37 +0200

    feat: create models based of IResponseMessage feat: create models based of IRequestMessage

--- libs/transport/abstractions/src/lib/message.request.interface.ts
+++ libs/transport/abstractions/src/lib/message.request.interface.ts
@@ -3,3 +3,15 @@ export interface IRequestMessage {
   messageType: string;
   payload: any;
 }
+
+export abstract class RequestMessage<T = any> implements IRequestMessage {
+  public messageId: string;
+  public messageType: string;
+  public payload: T;
+
+  constructor(args: Partial<IRequestMessage> = {}) {
+    this.messageId = args.messageId;
+    this.messageType = args.messageType;
+    this.payload = args.payload;
+  }
+}
--- libs/transport/abstractions/src/lib/message.response.interface.ts
+++ libs/transport/abstractions/src/lib/message.response.interface.ts
@@ -2,3 +2,13 @@ export interface IResponseMessage {
   sourceMessageId: string;
   payload: any;
 }
+
+export class ResponseMessage<T = any> implements IResponseMessage {
+  public sourceMessageId: string;
+  public payload: T;
+
+  constructor(args: Partial<IResponseMessage> = {}) {
+    this.sourceMessageId = args.sourceMessageId;
+    this.payload = args.payload;
+  }
+}
