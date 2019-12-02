36e58cdbfe873e213f411e6645c06628ef949cec
Ivor Thord-Gray <ivor.thord-gray@iotnxt.com>
2019-12-02 12:48:05 +0200

    chore: redo components library

--- libs/ui/components/text-box/README.md
+++
@@ -1,7 +0,0 @@
-# ui-components-text-box
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `ng test ui-components-text-box` to execute the unit tests.
--- libs/ui/components/text-box/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ui-components-text-box',
-  preset: '../../../../jest.config.js',
-  coverageDirectory: '../../../../coverage/libs/ui/components/text-box',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
