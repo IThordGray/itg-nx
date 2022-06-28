3c4ecb65603575bef1b7fe0010468a2039864693
Ivor Thord-Gray <ivor@agrigistics.co.za>
2022-06-28 19:51:13 +0200

    .

--- libs/frontend/notify/src/lib/abstractions/notify-provider.interface.ts
+++ libs/frontend/notify/src/lib/abstractions/notify-provider.interface.ts
@@ -1,10 +1,10 @@
 export interface INotifyProvider {
 
-  showNotificationAsync(title: string, message: string, args: Record<string, unknown>): Promise<void>;
-
   showAlertAsync(message: string, args: Record<string, unknown>): Promise<void>;
 
   showConfirmAsync(message: string, args: Record<string, unknown>): Promise<boolean>;
+
+  showNotificationAsync(title: string, message: string, args: Record<string, unknown>): Promise<void>;
 
   showPromptAsync(message: string, args: Record<string, unknown>): Promise<unknown>;
 
