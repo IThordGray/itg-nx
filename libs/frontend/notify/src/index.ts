e053a2246394cbcb4b6042be88c3b3502a78dc45
Ivor Thord-Gray <ivor.thordgray@gmail.com>
2022-10-25 14:10:25 +0200

    Update notify package

--- libs/frontend/notify/src/index.ts
+++ libs/frontend/notify/src/index.ts
@@ -1,2 +1,6 @@
+export * from './lib/abstractions/alert-options.interface';
+export * from './lib/abstractions/confirm-options.interface';
+export * from './lib/abstractions/notification-options.interface';
 export * from './lib/abstractions/notify-provider.interface';
+export * from './lib/abstractions/prompt-options.interface';
 export * from './lib/notify.service';
