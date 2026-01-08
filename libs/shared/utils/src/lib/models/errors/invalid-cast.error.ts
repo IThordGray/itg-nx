b8efe37b1a09beeffce53ab6ae6a072511877f87
IvorThord-Gray <ivor.thordgray@gmail.com>
2026-01-08 16:55:33 +0000

    Install Zod, Bump version

--- libs/shared/utils/src/lib/models/errors/invalid-cast.error.ts
+++ libs/shared/utils/src/lib/models/errors/invalid-cast.error.ts
@@ -1,3 +1,5 @@
+import { ZodError } from 'zod';
+
 export class InvalidCastError extends Error {
   constructor(objectName: string, readonly error: ZodError) {
     super(`Failed to cast object to ${objectName}.${error.issues.map(issue => `\n - ${issue.path.join('.')}: ${issue.message}`)}`);
